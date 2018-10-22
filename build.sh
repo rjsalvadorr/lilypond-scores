6a3259908fe817d21b9113acf4c4a7ea202b1d89
RJ Salvador <rjsalvadorr@gmail.com>
2018-10-21 21:52:08 -0400

    Added options to build script

--- build.sh
+++ build.sh
@@ -7,9 +7,31 @@ versionNumber = \"$versionNumber\"" > src/config.ly
 
 mkdir -p output
 cd src
-lilypond --output=../output/reference reference.ly
-lilypond --output=../output/blank-manuscript blank-manuscript.ly
-lilypond --output=../output/blank-manuscript-guitar blank-manuscript-guitar.ly
-lilypond --output=../output/blank-manuscript-piano blank-manuscript-piano.ly
-lilypond --output=../output/guitar-sketches guitar-sketches.ly
-lilypond --output=../output/guitar-pieces guitar-pieces.ly
+
+if [[ "$1" != "" && ("$1" == "--all" || "$1" == "-a") ]]; then
+  lilypond --output=../output/reference reference.ly
+  lilypond --output=../output/blank-manuscript blank-manuscript.ly
+  lilypond --output=../output/blank-manuscript-guitar blank-manuscript-guitar.ly
+  lilypond --output=../output/blank-manuscript-piano blank-manuscript-piano.ly
+  lilypond --output=../output/guitar-sketches guitar-sketches.ly
+  lilypond --output=../output/guitar-pieces guitar-pieces.ly
+elif [[ "$1" != "" && ("$1" == "--blanks" || "$1" == "-b") ]]; then
+  lilypond --output=../output/blank-manuscript blank-manuscript.ly
+  lilypond --output=../output/blank-manuscript-guitar blank-manuscript-guitar.ly
+  lilypond --output=../output/blank-manuscript-piano blank-manuscript-piano.ly
+elif [[ "$1" != "" && ("$1" == "--references" || "$1" == "-r") ]]; then
+  lilypond --output=../output/reference reference.ly
+elif [[ "$1" != "" && ("$1" == "--pieces" || "$1" == "-p") ]]; then
+  lilypond --output=../output/guitar-pieces guitar-pieces.ly
+elif [[ "$1" != "" && ("$1" == "--sketches" || "$1" == "-s") ]]; then
+  lilypond --output=../output/guitar-sketches guitar-sketches.ly
+else
+    echo ""
+    echo "Specify one of these options:"
+    echo "============================="
+    echo "-a | --all"
+    echo "-b | --blanks"
+    echo "-r | --references"
+    echo "-p | --pieces"
+    echo "-s | --sketches"
+fi
